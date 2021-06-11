4aac6ec0e3f0f80cd3ec26966f563f41d26971e5
josybarbosa04 <josenilda_silva123@outlook.com>
2021-06-11 14:22:14 -0300

    Mudança porta

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
     res.send("Josenilda Josenilda bunda fina");
 });
 
-app.listen(8080, () => {
-    console.log("Servidor iniciado na porta 8080: http://localhost:8080/");
+app.listen(4220, () => {
+    console.log("Servidor iniciado na porta 4220: http://localhost:4220/");
 });
