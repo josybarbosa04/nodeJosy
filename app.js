6e4147201038a77803fb2c8848a133bfceeef4a3
josybarbosa04 <josenilda_silva123@outlook.com>
2021-06-17 14:55:15 -0300

    Alterações feitas

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require ('express');
+const cors = require ('cors');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.options("*", cors());
 
-app.get("/", (req, res) => {
-    res.send("Josenilda Josenilda bunda fina");
+app.get("/", (req,res) => {
+
+    res.json({ mensagem: 'Josenilda Josenilda bunda fina' });
+});
+
+app.get("/josemires", (req,res) => {
+    res.json({ mensagem: 'Olá, josemires'});
+});
+
+app.post("/josemires", (req,res) => {
+    res.json({ mensagem: `Olá, ${req.body.nome} ${req.body.sobrenome} de altura ${req.body.altura}m e na idade ${req.body.idade}.
+     Filho da ${req.body.filiacao.mae} e do ${req.body.filiacao.pai}`});
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Servidor iniciado na porta ${PORT}: http://localhost:${PORT}/`);
+
+app.listen(PORT,() => {
+    console.log(`Servido iniciado na porta ${PORT}: http://localhost:${PORT}/`);
 });
+
+
+// GET =consulta
+// POST - criando
+// PUT - alterando
+// DELETE - excluir
